eea066f2c42709f06c295bd7a9d05f08067bbfe4
Ayush <114686508+1ayushp@users.noreply.github.com>
2024-05-27 15:39:14 +0000

    Changes to be committed: 	modified:   Mis/settings.py

--- Mis/settings.py
+++ Mis/settings.py
@@ -26,8 +26,8 @@ load_dotenv()
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i75vtz8l5%&tul*qm8d6^5#wrcj6cl#6u06+45rfm9wvlb5#7b'
-# SECRET_KEY = os.getenv("SECRET_KEY")
+
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
